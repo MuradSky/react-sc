d020a88ef785f5e8d8e125bcb4d7785c561dafe8
Murad <skytechnic.music@gmail.com>
2021-12-20 00:42:40 +0300

    Second Commit

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+const styles = {
+    fontFamily: 'Arial',
+    fontSize: '60px',
+    textAlign: 'center',
+    paddingTop: '100px',
+}
+
+export const App = () => (
+    <div className="App">
+        <h1 style={styles}>Hello Soundcloud</h1>
+    </div>
+)
