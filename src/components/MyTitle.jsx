46cb15fbae29c82a898018f652abaf4465016bd6
Murad <skytechnic.music@gmail.com>
2021-12-23 15:50:24 +0300

    New edits

---
+++ src/components/MyTitle.jsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+  return <h1>Hello component</h1>;
+}
+
+export default MyTitle;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,16 @@
 const styles = {
-    fontFamily: 'Arial',
-    fontSize: '60px',
-    textAlign: 'center',
-    paddingTop: '100px',
+  fontFamily: 'Arial',
+  fontSize: '60px',
+  textAlign: 'center',
+  paddingTop: '100px',
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={styles}>Hello Soundcloud</h1>
+    </div>
+  );
 }
 
-export const App = () => (
-    <div className="App">
-        <h1 style={styles}>Hello Soundcloud</h1>
-    </div>
-)
+export default App;
