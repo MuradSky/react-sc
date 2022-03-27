ee225b16dc418cd665eeb904c7bcb176f5f1037d
Murad <skytechnic.music@gmail.com>
2022-03-27 15:53:52 +0300

    new

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,28 +1,9 @@
-import React, { useState } from 'react';
-import TestTypeScriptProps from './TestTypeScriptProps';
-
-interface Data {
-    hello: string;
-    year: number;
-    count: number;
-    increment: () => void;
-}
+import React from 'react';
 
 function App() {
-    const [count, setCount] = useState<number>(0);
-    const handleClick = () => setCount((state) => state + 1);
-
-    const state: Data = {
-        hello: 'Привет TS',
-        year: 2022,
-        count: count,
-        increment: handleClick,
-    };
-
     return (
         <div className="App">
             <h1>Hello Soundcloud</h1>
-            <TestTypeScriptProps data={state} />
         </div>
     );
 }
